6c9c7c8d9804d362bb6524f330d8b516327ddc9f
Radek <radoslaw86n@gmail.com>
2017-09-08 10:00:18 +0100

    v.0.0.3a

--- WorkOffice/src/FamilyModel.java
+++ WorkOffice/src/FamilyModel.java
@@ -1,8 +1,39 @@
 
 public class FamilyModel
 {
-	private String name,surname,city,street,info;
+	private String name,surname,city,street,infofitness,language,experience,physicawork,employeeage;
 	private int id,birthdate,phone,postcode,nrhouse,nrflat,phonetofamily;
 	private double rate;
 
+// STRING	
+	 public String getName() {
+	        return name;
+	    }
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	    
+	 public String getSurname() {
+	        return surname;
+	    }
+	    public void setSurname(String surname) {
+	        this.surname = surname;
+	    }
+	    
+	 public String getCity() {
+	        return city;
+	    }
+	    public void setCity(String city) {
+	        this.city = city;
+	    }
+
+	    
+//INTEGER
+	    public int getId() {
+	        return id;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	 
 }
